ac9e13907a63846051bf780878063a423eb42970
HeslerCuevas <176766870+HeslerCuevas@users.noreply.github.com>
2024-12-28 13:48:01 -0400

    Add files via upload

---
+++ Part 1: The Basic/Repairing the Clocktower/Repairing the Clocktower/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number: ");
+var number = Convert.ToInt32(Console.ReadLine());
+int remainder = number % 2;
+if (number % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
