3e00ebc0e88c3aacc05f539a6fa0123506d60e1d
HeslerCuevas <176766870+HeslerCuevas@users.noreply.github.com>
2024-12-29 15:38:48 -0400

    Delete Part 1: The Basic directory

--- Part 1: The Basic/EXPERIMENT/Experiment/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-count(10);
-int count(int number)
-    {
-        Console.WriteLine(number);
-        if (number == 1) return 1;
-        return count(number - 1);
-    }
--- Part 1: The Basic/The Four Sisters and The Duckbear/The Four Sisters/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Eggs gathered: ");
-var eggs_gathered = Console.ReadLine();
-var eggsForSisters = Convert.ToInt32(eggs_gathered) / 4;
-var eggsForDuckbear = Convert.ToInt32(eggs_gathered) % 4;
-Console.WriteLine("Eggs for sisters: " + eggsForSisters + ", eggs for Duckbear: " + eggsForDuckbear);
