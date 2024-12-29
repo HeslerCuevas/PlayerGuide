d195bebcaaadeae9c3c40a16d7fac696f2ae4646
Hesler Cuevas <176766870+HeslerCuevas@users.noreply.github.com>
2024-12-29 15:37:30 -0400

    Testing

--- Part 3/CharberryTrees/Program.cs
+++ Part 3/CharberryTrees/Program.cs
@@ -21,7 +21,7 @@
             public event Action Ripened = () => { };
             public void MaybeGrow()
             {
-                // Only a tiny chance of ripening each time, but we try a lot! LET'S TESTSDADSA
+                // Only a tiny chance of ripening each time, but we try a lot!
                 if (_random.NextDouble() < 0.00000001 && !Ripe)
                 {
                     Ripe = true;
