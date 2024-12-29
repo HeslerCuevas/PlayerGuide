baff90f866cb71eeec4f9c708c24c09c469d2c1e
HeslerCuevas <176766870+HeslerCuevas@users.noreply.github.com>
2024-12-29 13:01:44 -0400

    Add files via upload

---
+++ Part 2: Object Oriented Programming/Simula's Test/Program.cs
@@ -0,0 +1,32 @@
+﻿void main()
+{
+    chestState chest = chestState.locked;
+    string action;
+
+    do
+    {
+        Console.Write($"The chest is {chest}. What do you want to do? ");
+        action = Console.ReadLine();
+
+        if (action == "open" && chest == chestState.unlocked)
+        {
+            chest = chestState.open;
+        }
+        else if (action == "close" && chest == chestState.open)
+        {
+            chest = chestState.unlocked;
+        }
+        else if (action == "unlock" && chest == chestState.locked)
+        {
+            chest = chestState.unlocked;
+        }
+        else if (action == "lock" && chest == chestState.unlocked)
+        {
+            chest = chestState.locked;
+        }
+    }
+    while (true);
+}
+main();
+
+enum chestState {locked, unlocked, open}
---
+++ Part 2: Object Oriented Programming/The Point/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ThePoint_Program
+{
+    class Point
+    {
+        public float CoordinateX { get; set; }
+        public float CoordinateY { get; set; }
+
+        //Constructor Parameters
+        public Point(float coordinateX, float coordinateY)
+        {
+            this.CoordinateX = coordinateX;
+            this.CoordinateY = coordinateY;
+        }
+
+        //Constructor Parameterless
+        public Point()
+        {
+            this.CoordinateX = 0;
+            this.CoordinateY = 0;
+        }
+    }
+
+    class method
+    {
+        static void Main()
+        {
+            Point point1 = new Point(2, 3);
+            Point point2 = new Point(-4, 0);
+
+            Console.WriteLine($"Point one is at coordinate ({point1.CoordinateX}, {point2.CoordinateY})");
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
